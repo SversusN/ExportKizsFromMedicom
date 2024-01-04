50e33f8bf367fbb215622177ffd4c3bcd2bb9033
vg_varankin <skaldnord@yandex.ru>
2024-01-04 16:39:17 +0300

    Добавьте файлы проекта.

---
+++ DBSetToCsv.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportKizsFromMedicom
+{
+    public static class DBSetToCsv
+    {
+        internal static void ExportDataSetToCsvFile(DataSet _DataSet, string DestinationCsvDirectory, out int progress, out int allRows, out string fileName)
+        { 
+            progress = 0;
+            allRows = _DataSet.Tables[0].Rows.Count;
+            fileName = string.Empty;
+            try
+            {
+                foreach (DataTable DDT in _DataSet.Tables)
+                {
+                    String MyFile = @DestinationCsvDirectory + "\\KizRemains"  + DateTime.Now.ToString("yyyyMMddhhMMssffff") + ".csv";//+ DateTime.Now.ToString("ddMMyyyyhhMMssffff")
+                    fileName = MyFile;
+                    using (var outputFile = File.CreateText(MyFile))
+                    {
+                        String CsvText = string.Empty;
+
+                        foreach (DataColumn DC in DDT.Columns)
+                        {
+                            if (CsvText != "")
+                                CsvText = CsvText + ";" + DC.ColumnName.ToString();
+                            else
+                                CsvText = DC.ColumnName.ToString();
+                        }
+                        outputFile.WriteLine(CsvText.ToString().TrimEnd(';'));
+                        CsvText = string.Empty;
+                        
+                        foreach (DataRow DDR in DDT.Rows)
+                        {
+                            foreach (DataColumn DCC in DDT.Columns)
+                            {
+                                if (CsvText != "")
+                                    CsvText = CsvText + ";" + DDR[DCC.ColumnName.ToString()].ToString();
+                                else
+                                    CsvText = DDR[DCC.ColumnName.ToString()].ToString();
+                            }
+                            outputFile.WriteLine(CsvText.ToString().TrimEnd(';'));
+                            CsvText = string.Empty;
+                            progress += 1;
+                        }
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+        }
+    }
+}
---
+++ ExportKizs.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExportKizsFromMedicom
+{
+    public partial class ExportKizs : Form
+    {
+        public ExportKizs()
+        {
+            InitializeComponent();
+            connectionString = textBox1.Text;
+
+        }
+        private string connectionString;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string constr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Bases;Extended Properties=dBASE IV";
+
+            //string connString = @"Provider=vfpoledb.1;Data Source=C:\Bases;Exclusive=false;Nulls=false";
+            string connString = connectionString;
+            using (OleDbConnection con = new OleDbConnection(connString))
+            {
+                var sql = "select pm.cim,p.nomk_ls, p.series, p.price from " + @"productmark.dbf pm"+" inner join " + @"product.dbf p" + " on p.idproduct = pm.idproduct where pm.ko_src > 0";
+                OleDbCommand cmd = new OleDbCommand(sql, con);
+                con.Open();
+                DataSet ds = new DataSet(); ;
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                da.Fill(ds);
+                string destinationFile = Environment.CurrentDirectory;// + "\\" + "sample.csv";
+                int progress = 0;
+                int allRows = 0 ;
+                string fileName = string.Empty;
+                DBSetToCsv.ExportDataSetToCsvFile(ds, destinationFile,out progress,out allRows, out fileName);
+                progressBar1.Maximum = allRows;
+                progressBar1.Value = progress;
+                MessageBox.Show($"Выгружен файл {fileName}");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            connectionString = textBox1.Text;
+        }
+    }
+}
+
