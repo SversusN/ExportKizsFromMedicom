1f7888be93088cbdbf47089a22c695c28f9260f0
vg_varankin <skaldnord@yandex.ru>
2024-01-04 17:30:58 +0300

    #delete unsed imports

--- ExportKizs.cs
+++ ExportKizs.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Data.Odbc;
 using System.Data.OleDb;
-using System.Diagnostics;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ExportKizsFromMedicom
